5b717cd1acb71795ac052a15fd16a6294a96fe61
Jonathan Shepardson <jonshepardson@gmail.com>
2016-01-10 16:34:28 -0800

    Simple wifi scripts to turn a device off and then back on in either Ad-Hoc or Monitor Mode.   ./script.sh <devicename>

---
+++ init_wifi_ad-hoc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+devname=$1
+
+if [ $# -eq 0 ]
+then
+	echo "usage: sh init_wifi_ad-hoc.sh wifi_device_name"
+	exit 1
+fi
+
+if [ "$(whoami)" != "root" ]
+then
+	echo "script must be run as root"
+	exit 1
+fi
+
+if [ -f /usr/bin/systemctl ] #systemd
+then
+	systemctl stop NetworkManager.service
+	systemctl stop NetworkManager.service
+fi
+
+if [ -f /usr/sbin/service ] #upstart
+then
+	service network-manager stop
+fi
+
+ip link set $devname down && sleep 1
+iw dev $devname set type ibss && sleep 1 #ibss = Ad-hoc
+ip addr add dev $devname 10.9.0.0/8 && sleep 1 #default ip address
+ip link set $devname up && sleep 1
+iw $devname ibss join rockets 2412 #default essid "rockets"
+
---
+++ init_wifi_monitor.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+devname=$1
+
+if [ $# -eq 0 ]
+then
+	echo "usage: sh init_wifi_ad-hoc.sh wifi_device_name"
+	exit 1
+fi
+
+if [ "$(whoami)" != "root" ]
+then
+	echo "script must be run as root"
+	exit 1
+fi
+
+if [ -f /usr/bin/systemctl ] #systemd
+then
+	systemctl stop NetworkManager.service
+	systemctl stop NetworkManager.service
+fi
+
+if [ -f /usr/sbin/service ] #upstart
+then
+	service network-manager stop
+fi
+
+ip link set $devname down && sleep 1
+iw dev $devname set monitor none && sleep 1 #monitor none = monitor with no flags
+ip link set $devname up && sleep 1
+
+
