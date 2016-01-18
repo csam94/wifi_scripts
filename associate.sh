fd775036f5ea6e295a18b8f9aecf77dcebc4c907
patrick <snixromero@gmail.com>
2016-01-18 13:16:17 -0800

    added associate script

---
+++ associate.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+devname=$1
+
+if [ $# -eq 0 ]
+then
+	echo "usage: sh init_wifi_ad-hoc.sh wifi_device_name"
+	exit 1
+fi
+
+if [ "$(whoami)" != "root" ]
+then
+	echo "script must be run as root"
+	exit 1
+fi
+
+if [ -f /usr/bin/systemctl ] #systemd
+then
+	systemctl stop NetworkManager.service
+	systemctl stop NetworkManager.service
+fi
+
+if [ -f /usr/sbin/service ] #upstart
+then
+	service network-manager stop
+fi
+
+
+ip link set $devname down && sleep 1 #set device down
+ip addr flush dev $devname && sleep 1 #flush previous config
+ip addr add 10.0.0.10 dev $devname #assign static ip
+ip link set $devname up && sleep 1 #set device up
+iwconfig $devname essid "rockets"
+
+
+
+#################
+# DISCLAIMER!!!!
+######untested#######
